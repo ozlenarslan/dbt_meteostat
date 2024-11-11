f7b930ad1c3bf79b499a40b3a996b2fbadc897e8
Oz <oz@MacBook-Pro-von-Oz.local>
2024-11-11 12:56:16 +0100

    comment

--- models/prep/prep_weather_daily.sql
+++ models/prep/prep_weather_daily.sql
@@ -1,25 +1,25 @@
 WITH daily_data AS (
-    SELECT * 
-    FROM {{ref('staging_weather_daily')}}
+    SELECT *
+    FROM {{ ref('staging_weather_daily') }}
 ),
 add_features AS (
     SELECT *
-		, ... AS date_day
-		, ... AS date_month
-		, ... AS date_year
-		, ... AS cw
-		, ... AS month_name
-		, ... AS weekday
-    FROM daily_data 
+        , DATE_PART('day', date) AS date_day
+        , DATE_PART('month', date) AS date_month
+        , DATE_PART('year', date) AS date_year
+        , EXTRACT(WEEK FROM date) AS cw  -- Adjusted for general SQL compatibility
+        , TO_CHAR(date, 'Month') AS month_name  -- For databases like PostgreSQL
+        , TO_CHAR(date, 'Day') AS weekday       -- For databases like PostgreSQL
+    FROM daily_data
 ),
 add_more_features AS (
     SELECT *
-		, (CASE 
-			WHEN month_name in ... THEN 'winter'
-			WHEN ... THEN 'spring'
-            WHEN ... THEN 'summer'
-            WHEN ... THEN 'autumn'
-		END) AS season
+        , (CASE
+            WHEN DATE_PART('month', date) IN (12, 1, 2) THEN 'winter'
+            WHEN DATE_PART('month', date) IN (3, 4, 5) THEN 'spring'
+            WHEN DATE_PART('month', date) IN (6, 7, 8) THEN 'summer'
+            WHEN DATE_PART('month', date) IN (9, 10, 11) THEN 'autumn'
+        END) AS season
     FROM add_features
 )
 SELECT *
